ee6e595579b9f89b3356a68a7fd760ebb8d38f4f
TiagoArriegas <tiago.arriegas@aebenfica.org>
2018-04-29 12:45:51 +0100

    5

--- batalhanaval.js
+++ batalhanaval.js
@@ -1,12 +1,12 @@
 
-    var posicaoAleatoria = 1 + Math.floor(Math.random() * 5);
+    var posicaoAleatoria = 1 + Math.floor(Math.random() * 28);
 
     var parte1 = posicaoAleatoria;
-    var parte2 = parte1+1;
+    var parte2 = parte1 +1;
     var parte3 = parte2 +1;
+	var parte4 = parte3 +1;
 
-	
-var jogada;
+	var jogada;
 var atingidas = 0;
 var totalJogadas = 0;
 
@@ -17,7 +17,7 @@ while (foiAfundado == false) {
 
    jogada = prompt("Podes jogar - Escolhe um numero entre 1 e 7:");
 
-   if (jogada < 1 || jogada > 7) {
+   if (jogada < 1 || jogada > 30) {
 
       alert("Jogada inválida. Tenta de novo!");
 
@@ -25,13 +25,13 @@ while (foiAfundado == false) {
 
       totalJogadas = totalJogadas + 1;
 
-      if (jogada == parte1 || jogada == parte2 || jogada == parte3) {
+      if (jogada == parte1 || jogada == parte2 || jogada == parte3 || jogada ==parte4) {
 
          alert("Acertaste em mais uma parte!");
 
          atingidas = atingidas +1;
 
-         if (atingidas == 3) {
+         if (atingidas == 4) {
 
             foiAfundado = true;
 
@@ -50,6 +50,6 @@ while (foiAfundado == false) {
 }
 
 var estatistica = "Foi preciso um total de " + totalJogadas + " jogadas para afundar o navio, " + 
-                          "o que quer dizer que a tua pontaria foi de " + (3/totalJogadas);
+                          "o que quer dizer que a tua pontaria foi de " + (4/totalJogadas);
 
 alert(estatistica);
